a1bd80d40fff22a5b04ac68c681e37579483363d
shubham sharma <shubham.sharma@venturepact.com>
2025-02-12 14:59:54 +0530

    add

--- services/reportService.js
+++ services/reportService.js
@@ -99,14 +99,8 @@ exports.generateReport = async (user, reportType, headId) => {
     // Launch Puppeteer and generate PDF
     // const browser = await puppeteer.launch(); // For Testing or Development purpose
     const browser = await puppeteer.launch({
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-gpu',
-        '--disable-dev-shm-usage'
-      ],
-      headless: "new", // Run in headless mode,
-      executablePath: "/usr/bin/chromium-browser"
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      headless: true // Run in headless mode
     });
 
     // Render the EJS template to HTML
